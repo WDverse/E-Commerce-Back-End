2289198bc68e36789222048ad2d62b7f92669c56
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-20 01:04:59 -0400

    fixed update and delete routes

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: null
+  onDelete: 'SET NULL',
 });
 
 // Categories have many Products
@@ -18,14 +18,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag',
+  through: ProductTag,
   foreignKey: 'product_id',
-  onDelete: null,
+  onDelete: 'SET NULL',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag',
+  through: ProductTag,
   foreignKey: 'tag_id',
 });
 
